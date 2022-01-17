7189ba048793678538f61ba049a2fff50d04b935
1250498077 <asd912345qq.com@qq.coml>
2022-01-17 22:00:12 +0800

    nginx

--- main/src/App.vue
+++ main/src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div id="app">
-    <h1>VUE</h1>
+    <h1 class="vue-title">VUE</h1>
     <img src="./assets/logo.png">
     <h1>REACT</h1>
     <router-link to="/Dog">Dog</router-link>
     <router-link to="/Cat">Cat</router-link>
     <router-view></router-view>
+    <br />
+    <button v-on:click="editUrl3000">请求'//localhost:3000'</button>
+    <button v-on:click="editUrl3001">请求'//localhost:3001'</button>
+    <button v-on:click="initApp">加载react16</button>
     <br />
     <!-- <button v-on:click="childrenToFather">清除沙箱</button> -->
     <div>{{store ? store.state.visible : 0}}</div>
@@ -19,6 +23,7 @@
 </template>
 
 <script>
+import './index.css';
 // 快照沙箱
 import { SnapShotSandbox } from './snapShotSandbox'
 // 代理沙箱
@@ -26,14 +31,14 @@ import { ProxySandbox } from './proxySandbox'
 // 子应用
 let common = {};
 let vueComponentObj = {};
-const url = '//localhost:3000';
 const fetchResource = (url) => {
   return fetch(url).then(async (res) => {
     return await res.text()
   })
 };
-const getResource = async (root) => {
+const getResource = async (root, url) => {
   const scriptUrl = [];
+  const cssUrl = [];
   const script = [];
   const dom = root.outerHTML
 
@@ -68,6 +73,14 @@ const getResource = async (root) => {
           scriptUrl.push(`http:${url}${href}`)
         }
       }
+      // if (href.endsWith('.css')) {
+      //   console.log('加载css')
+      //   if (href.startsWith('http')) {
+      //     cssUrl.push(href)
+      //   } else {
+      //     cssUrl.push(`http:${url}${href}`)
+      //   }
+      // }
     }
 
     for (let i = 0; i < children.length; i++) {
@@ -78,7 +91,6 @@ const getResource = async (root) => {
   deepParse(root)
   return [dom, scriptUrl, script]
 }
-
 
 // 执行子应用的js字符
 const performScriptForFunction = (script, global) => {
@@ -108,54 +120,69 @@ export default {
   components: {},
   data: () => {
     return {
-      componentObj: {}
+      componentObj: {},
+      url: '//localhost:3000'
     }
   },
   mounted: async function () {
-    window.__MICRO_WEB__ = true;
-    // 子应用的所有js字符
-    let allScript = [];
-    /* 创建一个沙箱：有些子应用有挂载的全局变量，我们在渲染
-       子应用的时候要准备好那些全局变量，同时暂存下主应用的全局变量
-       当渲染子应用卸载的时候，我们要恢复现场，所以沙箱在这里是为了
-       恢复现场用的 */
-    common.proxy = new ProxySandbox();
-
-
-    // 拿到子应用的index.html
-    const html = await fetchResource(url);
-    // 创建一个装载子应用的index.html的div
-    const div = document.createElement('div');
-    // 拿到的字符含有<html>、<header>、<body>标签,插入进去之后浏览器会自动帮助我们删除
-    div.innerHTML = html;
-
-
-    // 解析刚刚拿到的子应用的div
-    // scriptUrl： 拿到子应用的所有依赖的js文件的url
-    // script： 拿到子应用的内嵌的js逻辑
-    const [dom, scriptUrl, script] = await getResource(div);
-
-
-    // 拿到子应用的 index.html 的外链的js
-    const fetchedResource = await Promise.all(scriptUrl.map(async (item) => {
-      return fetchResource(item);
-    }));
-    // 拼合子应用的 index.html 的内联js和外链js
-    allScript = script.concat(fetchedResource);
-    // 执行子应用的javascript
-    allScript.forEach(item => {
-      const life = performScriptForFunction(item, common.proxy.proxy);
-      if (life && life.mount) {
-        child = life;
-        this.setModalType = life.setModalType
-        // 将父组件方法传递到子组件，子组件可以借此向父组件传递信息
-        life.mount({
-          childrenToFather: this.childrenToFather
-        });
-      }
-    });
+    // this.initApp()
   },
   methods: {
+    editUrl3000: () => {
+      this.url = '//localhost:3000'
+    },
+    editUrl3001: () => {
+      this.url = '//localhost:3002'
+    },
+    initApp: async () => {
+      window.__MICRO_WEB__ = true;
+      // 子应用的所有js字符
+      let allScript = [];
+      /* 创建一个沙箱：有些子应用有挂载的全局变量，我们在渲染
+        子应用的时候要准备好那些全局变量，同时暂存下主应用的全局变量
+        当渲染子应用卸载的时候，我们要恢复现场，所以沙箱在这里是为了
+        恢复现场用的 */
+      common.proxy = new ProxySandbox();
+
+
+      // 拿到子应用的index.html
+      const html = await fetchResource(this.url);
+      // 创建一个装载子应用的index.html的div
+      const div = document.createElement('div');
+      // 拿到的字符含有<html>、<header>、<body>标签,插入进去之后浏览器会自动帮助我们删除
+      div.innerHTML = html;
+
+
+      // 解析刚刚拿到的子应用的div
+      // scriptUrl： 拿到子应用的所有依赖的js文件的url
+      // script： 拿到子应用的内嵌的js逻辑
+      const [dom, scriptUrl, script, cssUrl] = await getResource(div, this.url);
+      console.log('scriptUrl', scriptUrl)
+      // // 拿到子应用的 index.html 的外链的js
+      // const cssFetchedResource = await Promise.all(cssUrl.map(async (item) => {
+      //   return fetchResource(item);
+      // }));
+
+      // 拿到子应用的 index.html 的外链的js
+      const fetchedResource = await Promise.all(scriptUrl.map(async (item) => {
+        return fetchResource(item);
+      }));
+      // 拼合子应用的 index.html 的内联js和外链js
+      allScript = script.concat(fetchedResource);
+      console.log('allScript', allScript)
+      // 执行子应用的javascript
+      allScript.forEach(item => {
+        const life = performScriptForFunction(item, common.proxy.proxy);
+        if (life && life.mount) {
+          child = life;
+          this.setModalType = life.setModalType
+          // 将父组件方法传递到子组件，子组件可以借此向父组件传递信息
+          life.mount({
+            childrenToFather: this.childrenToFather
+          });
+        }
+      });
+    },
     clearSandBox: () => {
       common.proxy.inactive();
     },
@@ -165,22 +192,39 @@ export default {
         visible: store.state.visible + 1
       })
     },
+    reactContainer: function(type, props = {
+      handleOk: () => {},
+      handleCancel: () => {},
+      componentLoadCallback: () => {}
+    }) {
+      console.log('vueComponentObj', vueComponentObj)
+      if (vueComponentObj && vueComponentObj.componentObj && vueComponentObj.type === type) {
+         vueComponentObj.componentObj.showModal(true);
+      } else {
+        child.setModalType(type, {
+          handleOk: props.handleOk,
+          handleCancel: props.handleCancel,
+          componentLoadCallback: props.componentLoadCallback
+        });
+      }
+    },
     openModal1: function() {
-      console.log('openModal1')
-      child.setModalType('Modal1', {
-        handleOk: () => { console.log('handleOk') },
-        handleCancel: () => { console.log('handleCancel') },
+      this.reactContainer('Modal1', {
+        handleOk: () => {  vueComponentObj.componentObj.closeModal() },
+        handleCancel: () => { vueComponentObj.componentObj.closeModal() },
         componentLoadCallback: (componentObj) => {
-          vueComponentObj = componentObj; 
-          vueComponentObj.showModal(true);
+          vueComponentObj = {
+            componentObj: componentObj,
+            type: 'Modal1'
+          }; 
+          vueComponentObj.componentObj.showModal(true);
         }
-      });
-      
+      })
     },
     openModal2: function() {
       child.setModalType('Modal2', {
-        handleOk: () => { console.log('handleOk') },
-        handleCancel: () => { console.log('handleCancel') },
+        handleOk: () => { vueComponentObj.closeModal() },
+        handleCancel: () => { vueComponentObj.closeModal() },
         componentLoadCallback: (componentObj) => { 
           vueComponentObj = componentObj;
           vueComponentObj.showModal(true);
@@ -206,4 +250,4 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-</style>
+</style> 
